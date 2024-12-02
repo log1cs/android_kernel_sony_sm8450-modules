b85dc2354604b5ffd9433f7bbbf88e4354deb73e
Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
2024-12-02 23:09:46 +0000

    wlan: qcacmn: Disable all of qdf_trace when WLAN_DEBUG is disabled
    Some parts of qcacld-3.0 still print their garbage to dmesg even when
    WLAN_DEBUG is disabled because the qdf_trace utilities are still
    available. Compiling out qdf_trace when WLAN_DEBUG is off fixes that up
    nicely.
    
    Change-Id: Ia34868442f11176bb663d8a5ef4ccb8a2fc96595
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- qcom/opensource/wlan/qca-wifi-host-cmn/qdf/linux/src/qdf_trace.c
+++ qcom/opensource/wlan/qca-wifi-host-cmn/qdf/linux/src/qdf_trace.c
@@ -23,6 +23,7 @@
  *  Trace, logging, and debugging definitions and APIs
  */
 
+#ifdef WLAN_DEBUG
 /* Include Files */
 #include "qdf_str.h"
 #include <qdf_trace.h>
@@ -4422,4 +4423,4 @@ void __qdf_bug(void)
 qdf_export_symbol(__qdf_bug);
 #endif /* CONFIG_SLUB_DEBUG */
 #endif /* PANIC_ON_BUG */
-
+#endif
